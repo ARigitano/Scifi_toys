84932fda6c595cf2aa6cfef0557f02e11c453bd9
ARigitano <antoine@rigitano.com>
2024-10-24 20:18:05 +0200

    Props face forward when dropped on snapping surfaces

---
+++ Assets/Scenes/VRCDefaultWorldScene_UdonProgramSources/Prop.cs
@@ -0,0 +1,40 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+
+[RequireComponent(typeof(VRCPickup))]
+[RequireComponent(typeof(Rigidbody))]
+public class Prop : UdonSharpBehaviour
+{
+    private bool _isDropped = false; //Has the prop been dropped after having been picked up?
+    public bool isSnapped = false; //Has the prop entered a snapzone?
+    private bool _isActivated = false; //Has the prop came to life?
+
+    public override void OnDrop()
+    {
+        base.OnDrop();
+        _isDropped = true;
+        Debug.Log("dropped");
+    }
+
+    //Sends the event to activate the prop accross the network
+    public void ActivateNetwork()
+    {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Activate");
+    }
+
+    //Makes the prop come to life
+    public void Activate()
+    {
+        if (_isDropped && isSnapped)
+        {
+            _isActivated = true;
+            transform.rotation = Quaternion.LookRotation(Vector3.forward);
+            Debug.Log("ACTIVATED");
+        }
+    }
+}
---
+++ Assets/Scenes/VRCDefaultWorldScene_UdonProgramSources/Snapzone.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Snapzone : UdonSharpBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Prop>() != null)
+        {
+            other.gameObject.GetComponent<Prop>().isSnapped = true;
+            other.gameObject.GetComponent<Prop>().ActivateNetwork();
+            
+            Debug.Log("snapped");
+        }
+    }
+}
